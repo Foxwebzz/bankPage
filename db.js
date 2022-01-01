15af1b98c72933d3a6ab9c53a702cca27228f97b
DESKTOP-UG3PI5T\Novakovic <nikola2312017@gmail.com>
2022-01-01 18:37:50 +0100

    master

---
+++ db.js
@@ -0,0 +1,24 @@
+let db = [
+    {
+        id:    1,
+        first : "Danilo",
+        last : "Vesovic", 
+        deposit : "11000",
+        card : "Visa"
+    },
+    {
+        id:    2,
+        first : "Sasha",
+        last : "Vesovic", 
+        deposit : "11000",
+        card : "Visa"
+    },
+    {
+        id:    3,
+        first : "Marko",
+        last : "Vesovic", 
+        deposit : "11000",
+        card : "Visa"
+    },
+   
+];
